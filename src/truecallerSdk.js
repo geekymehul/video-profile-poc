f5b0b1da375be1d67605c21f9a5782ca5d95545d
mehul.rawat <mehul.rawat@infoedge.com>
2024-07-02 17:38:51 +0530

    changing the partner key.........

--- src/truecallerSdk.js
+++ src/truecallerSdk.js
@@ -24,22 +24,7 @@ const TrueCallerSdk =()=> {
         if(inputElem.current) {
           (inputElem.current).focus();
         }
-        window.location = `truecallersdk://truesdk/web_verify?
-          type=btmsheet
-          &requestNonce=${randomString}
-          &partnerKey=v2oqbaf8ba71d00594437acfcac38228cd6c6
-          &partnerName=Naukrigulf
-          &lang=en
-          &privacyUrl=https://www.naukrigulf.com/privacy-policy
-          &termsUrl=https://www.naukrigulf.com/terms-and-conditions
-          &loginPrefix=proceed
-          &loginSuffix=verifymobile
-          &ctaPrefix=proceedwith
-          &ctaColor=%230083da
-          &ctaTextColor=%23fff
-          &btnShape=round
-          &skipOption=later
-          &ttl=15000`;
+        window.location = `truecallersdk://truesdk/web_verify?type=btmsheet&requestNonce=${randomString}&partnerKey=NN1G957676d4c97204515b82ecef7b2d675fc&partnerName=Naukrigulf&lang=en&privacyUrl=https://www.naukrigulf.com/privacy-policy&termsUrl=https://www.naukrigulf.com/terms-and-conditions&loginPrefix=proceed&loginSuffix=verifymobile&ctaPrefix=proceedwith&ctaColor=%230083da&ctaTextColor=%23fff&btnShape=round&skipOption=later&ttl=15000`;
         setTimeout(function() {
           if( document.hasFocus() ){
             // Truecaller app not present on the device
