29f84a0ec4701ab31cfa880975c7a4eb5261e0a9
mehul.rawat <mehul.rawat@infoedge.com>
2024-07-12 14:45:13 +0530

    updatin app key for truecaller..

--- src/truecallerSdk.js
+++ src/truecallerSdk.js
@@ -24,7 +24,7 @@ const TrueCallerSdk =()=> {
         if(inputElem.current) {
           (inputElem.current).focus();
         }
-        window.location = `truecallersdk://truesdk/web_verify?type=btmsheet&requestNonce=${randomString}&partnerKey=NN1G957676d4c97204515b82ecef7b2d675fc&partnerName=Naukrigulf&lang=en&privacyUrl=https://www.naukrigulf.com/privacy-policy&termsUrl=https://www.naukrigulf.com/terms-and-conditions&loginPrefix=proceed&loginSuffix=verifymobile&ctaPrefix=proceedwith&ctaColor=%230083da&ctaTextColor=%23fff&btnShape=round&skipOption=later&ttl=15000`;
+        window.location = `truecallersdk://truesdk/web_verify?type=btmsheet&requestNonce=${randomString}&partnerKey=uTFaCdd1a1f00292d464fb039de4c79dbf791&partnerName=Naukrigulf&lang=en&privacyUrl=https://www.naukrigulf.com/privacy-policy&termsUrl=https://www.naukrigulf.com/terms-and-conditions&loginPrefix=proceed&loginSuffix=verifymobile&ctaPrefix=proceedwith&ctaColor=%230083da&ctaTextColor=%23fff&btnShape=round&skipOption=later&ttl=15000`;
         setTimeout(function() {
           if( document.hasFocus() ){
             // Truecaller app not present on the device
